ddf471e8df2c76a6c5cf716866e4734701636a7f
Hadrien Lanneau <hadrien.lanneau@wikiogroup.com>
2014-05-06 10:34:51 +0200

    Get users from db

--- src/models/users.js
+++ src/models/users.js
@@ -1,6 +1,8 @@
 var _ = require('lodash'),
+  client = require('../database/redisclient'),
   Q = require('q'),
-  Models = require('./models');
+  Models = require('./models'),
+  sha1 = require('sha1');
 
 function Users(config) {
   this.constructor.$_super.apply(this, arguments);
@@ -8,28 +10,48 @@ function Users(config) {
 Models.extend(Users);
 
 Users.checkAuth = function(username, password) {
-  return Q.fcall(function () {
-    if ('hadrien' === username &&
-        '123456' === password) {
-      return Users.get({uid: 1});
-    }
-  });
+  var deferred = Q.defer(),
+    uid;
+
+  Q.ninvoke(client, 'hget', 'indexes:user:username', username)
+    .then(function(data) {
+      if (!data) throw new Error('User not found');
+
+      uid = data;
+
+      return Users.get({uid: uid});
+    })
+    .then(function(user) {
+      if (user.password !== sha1(password)) throw new Error('Wrong Password!');
+
+      deferred.resolve(user);
+    })
+    .catch(function(err) {
+      deferred.reject(err);
+    });
+
+  return deferred.promise;
 };
 
 Users.get = function(config) {
+  var deferred = Q.defer();
+  
   config = config || {};
 
-  return Q.fcall(function() {
-    if (1 === config.uid) {
-      return new Users({
-        uid: 1,
-        username: 'hadrien',
-        firstname: 'Hadrien',
-        lastname: 'Lanneau'
+  if (config.uid) {
+    Q.ninvoke(client, 'hgetall', 'user:'+config.uid)
+      .then(function(data) {
+        data.uid = config.uid;
+        deferred.resolve(new Users(data));
+      })
+      .catch(function(err) {
+        deferred.reject(err);
       });
-    }
-    throw new Error('No user found');
-  });
+  } else {
+    deferred.reject('Bad params');
+  }
+
+  return deferred.promise;
 };
 
 module.exports = Users;
