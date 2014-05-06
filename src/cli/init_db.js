f171418adea4ed0eaf735c6eca9c35f9a03c5461
Hadrien Lanneau <hadrien.lanneau@wikiogroup.com>
2014-05-06 15:53:46 +0200

    Usefull script to delete all saved days for user

--- src/cli/init_db.js
+++ src/cli/init_db.js
@@ -1,9 +1,9 @@
 var fixtures = require('../database/fixtures'),
   _ = require('lodash'),
   args = process.argv.slice(2),
-  db = _.find(args, function(v) {
+  db = (_.find(args, function(v) {
     return '--db' === v.split('=')[0];
-  }).split('=')[1];
+  }) || '').split('=')[1];
 
 fixtures(db)
   .then(function (data) {
